2403336aea745bce2eddf8741430d21579144cf9
Sovch <Filin_999@bk.ru>
2022-11-07 17:49:53 +0400

    no cors

--- src/getDataByRequest.js
+++ src/getDataByRequest.js
@@ -2,13 +2,22 @@ import axios from 'axios';
 
 const getRssData = (url) => {
   console.log('url', url);
-  axios.get(url)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+
+  return axios(url, {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+    credentials: 'same-origin',
+  }).then(response => {
+    console.log('response', response);
+  })
+  .catch((error) => {
+    console.log('error', error);
+  });
 };
 
 export default getRssData;
